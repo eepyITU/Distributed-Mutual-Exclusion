77987146507840897ce1e60a9eb7e11a0c08943f
Annabell Philip Nørdam <apno@itu.dk>
2023-11-13 13:26:57 +0100

    trying to parse IPs in command line

--- node/node.go
+++ node/node.go
@@ -1,23 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 )
 
 // using os to try to begin to access ips from command line.
 func main() {
-	file, err := os.Open("text.txt")
-	if err != nil {
-		log.Fatal(err)
+	if len(os.Args) > 7 { //Shortest ip possible? (0.0.0.0)
+		log.Println("Your input needs to one or multiple valid IP addresses.")
+		os.Exit(400) //Http status code for wrong syntax?
+	} else {
+		log.Println("This is a valid IP!")
 	}
 
-	data := make([]byte, 100)
-	count, err := file.Read(data)
-	if err != nil {
-		log.Fatal(err)
-	}
+	//file, err := os.Open("text.txt")
+	//if err != nil {
+	//log.Fatal(err)
+	//}
 
-	fmt.Printf("read %d bytes: %q\n", count, data[:count])
+	//data := make([]byte, 100)
+	//count, err := file.Read(data)
+	//if err != nil {
+	//log.Fatal(err)
+	//}
+
+	//fmt.Printf("read %d bytes: %q\n", count, data[:count])
 }
