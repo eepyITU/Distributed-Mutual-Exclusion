dd48c15a9742f48df0ba7a3b201ec033274cb129
Annabell Philip Nørdam <apno@itu.dk>
2023-11-13 14:32:27 +0100

    little more command line args

--- node.go
+++ node.go
@@ -10,9 +10,22 @@ func main() {
 	if len(os.Args) > 7 { //Shortest ip possible? (0.0.0.0)
 		log.Println("Your input needs to one or multiple valid IP addresses.")
 		os.Exit(400) //Http status code for wrong syntax?
-	} else {
-		log.Println("This is a valid IP!")
 	}
+
+	//else {
+	//log.Println("This is a valid IP!")
+	//}
+
+	ipport := os.Args[1]
+	ipportnext := os.Args[2]
+	log.Println(ipport)
+	log.Println(ipportnext)
+
+	//give each node a corresponding id.
+	//that id needs to be something the other nodes will be able to become aware of.
+	//needs to be a critical section somewhere that each node needs to access at some point.
+	//Use a token ring to pass information in a circle, such that every node gets a turn.
+	//Use unlock and lock, to keep the information to only one node at a time.
 
 	//file, err := os.Open("text.txt")
 	//if err != nil {
